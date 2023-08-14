4a9aa7c7d44586e44ecb0d3b29a485464bd26d89
lameiragaspar <120721002+lameiragaspar@users.noreply.github.com>
2023-08-14 17:47:13 +0100

    Novo Desafio quaese pronto

---
+++ Exercícios/Ex03_aula_14/js/sctipt.js
@@ -0,0 +1,26 @@
+function contar(){
+    var ini = window.document.getElementById('i_inicio')
+    var inicio = Number(ini.value)
+    var fim = window.document.getElementById('i_fim')
+    var final = Number(fim.value)
+    var pas = window.document.getElementById('i_passo')
+    var passo = Number(pas.value)
+
+    var resultado = window.document.querySelector('div#res')
+    
+
+    if(ini.value.length == 0 || fim.value.length == 0){
+        window.alert('[Inpossível Contar!] Verifique os dados e tente novamente!')
+    }else if(pas.value.length == 0 || passo == 0){
+        window.alert('[PASSO INVÁLIDO!] Considerando passo [1]')
+        passo = 1
+    } else{
+        resultado.innerHTML = ''
+        if(inicio < final){
+            var cont = inicio
+            for(var cont = inicio; cont <= final; cont += passo){
+                resultado.innerHTML += `${cont}...`
+            }
+        }   
+    }     
+}
